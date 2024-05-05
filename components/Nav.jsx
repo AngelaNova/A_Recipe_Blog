3ad87df8b80825039a1bc90f481c07a453b36361
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-04 21:48:56 -0400

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -34,7 +34,7 @@ const Nav = () => {
       {/* Desktop Navigation */}
       <div className='sm:flex justify-in_between position:relative left-0 items-left'>
         {/* Sign In button or Sign out button and profile picture from google */}
-        {!session ? (
+        {(session === null || session === undefined)  ? (
           <div className='flex md:gap-5 justify-end items-center'>
             <button
               type='button'
@@ -84,7 +84,7 @@ const Nav = () => {
       {/* Mobile Navigation */}
       <div className='sm:flex relative'>
         {/* Display profile picture and dropdown if signed in */}
-        {session?.user && (
+        {(session === null || session === undefined)  && (
           <div className='flex'>
             
 
