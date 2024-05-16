991def86df1798c8375862ca86212b8561666a63
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-16 10:34:58 -0400

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3' position='absolute' top='70px' right='0px'>
       <Link href='./' className='flex gap-2 flex-center l'>
-        //logo + name of the website
+        {/* logo + name of the website*/}
         <Image
           src='/assets/images/logo.png'
           alt='Orange Blog logo'
