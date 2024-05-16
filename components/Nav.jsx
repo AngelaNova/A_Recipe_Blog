f798672b4cc1abbb608cb3b1db4a088e57253d1f
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-16 11:43:37 -0400

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
 
   
   return (
-    <nav className='flex-between w-full mb-16 pt-3' position='absolute' top='70px' right='0px'>
+    <nav className='flex-between w-full mb-16 pt-3 mt-5 align-items'>
       <Link href='./' className='flex gap-2 flex-center l'>
         {/* logo + name of the website*/}
         <Image
