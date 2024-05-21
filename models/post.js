f72ffbaa26f1ffba31ef95588de768e0a640eab8
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-21 10:23:02 -0400

    Update post.js

---
+++ models/post.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.models.Post || mongoose.model('Post', PostSchema);
+
+export default Post;
