aa76004273c798e0a198916ddd46e47a3ffb2c1f
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-20 15:00:49 -0400

    Update options.js

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -6,11 +6,80 @@ import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("Profile GitHub: ", profile);
+
+        let userRole = "GitHub User";
+        if (profile?.email == "jake@claritycoders.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_Secret,}),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
-  ],
+    }),
+  ({
+    name: "Credentials",
+    credentials: {
+      email: {
+        label: "email:",
+        type: "text",
+        placeholder: "your-email",
+      },
+      password: {
+        label: "password:",
+        type: "password",
+        placeholder: "your-password",
+      },
+    },
+    async authorize(credentials) {
+      try {
+        const foundUser = await User.findOne({ email: credentials.email })
+          .lean()
+          .exec();
+
+        if (foundUser) {
+          console.log("User Exists");
+          const match = await bcrypt.compare(
+            credentials.password,
+            foundUser.password
+          );
+
+          if (match) {
+            console.log("Good Pass");
+            delete foundUser.password;
+
+            foundUser["role"] = "Unverified Email";
+            return foundUser;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      return null;
+    },
+  })],
+  /*  might add after
+   callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
+*/
   callbacks: {       
     async session({ session }) {
       try {
