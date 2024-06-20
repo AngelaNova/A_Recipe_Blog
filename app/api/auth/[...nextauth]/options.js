01be8f1c73961ac5e3799c4c3a23fc06d1b958ee
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-20 15:28:08 -0400

    updated NextAuth and Server side
    Authentication and authorization

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
