8cdea4ec820b89296aecb2cd28cc90de2e23440d
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-23 14:52:53 -0400

    Update page.jsx

--- app/CreateUser/page.jsx
+++ app/CreateUser/page.jsx
@@ -1,11 +1,7 @@
 import UserForm from "../(components)/UserForm";
 
 const CreateUser = () => {
-  return (
-    <div>
-      <UserForm />
-    </div>
-  );
+  return <div>{/* <UserForm /> */}</div>;
 };
 
 export default CreateUser;
