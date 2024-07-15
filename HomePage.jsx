09206403b31e33718a73814c22ee90b8ee43861b
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-07-15 14:40:56 -0400

    Create HomePage.jsx

---
+++ HomePage.jsx
@@ -0,0 +1,21 @@
+import Feed from "@components/Feed.jsx";
+
+const HomePage = () => {
+  return (
+    <section className="w-full flex -center flex-col">
+      <h1 className="head_text text-center">
+        Discovery & Share
+        <br className="max-mid:hidden" />
+        <span className="orange_gradient">Best Recipes at Orange Blog</span>
+      </h1>
+      <p className="desc text-center">
+        Orange Blog is an recipe sharing tool for modern world to discover,
+        create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
+};
+
+export default HomePage;
